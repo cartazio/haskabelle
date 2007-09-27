ced39116ebc303bc5d17be34fa3741eb98ea4600
rittweil <devnull@localhost>
2007-09-27 19:07:38 +0000

    * Importer.Preprocess:
        Also globalizes let expressions now.
    
    * Importer.Convert:
    
        Fail when encountering a let expression, as all occurences should
        have been preprocessed away.
    
    * Importer.Utilities.Hsx (alphaconvert)
    
        New instance declaration for HsRhs.

--- importer/Preprocess.hs
+++ importer/Preprocess.hs
@@ -4,7 +4,9 @@ module Importer.Preprocess (preprocessHsModule) where
 import Control.Monad.State
 import Language.Haskell.Hsx
 
-import Importer.Utilities.Misc (concatMapM)
+import Data.Generics.Biplate
+
+import Importer.Utilities.Misc -- (concatMapM, assert)
 import Importer.Utilities.Hsx (bindingsFromDecls, alphaconvert)
 import Importer.Utilities.Gensym
 
@@ -40,20 +42,26 @@ delocalizeDecl :: Module -> HsDecl -> State GensymCount [HsDecl]
 
 delocalizeDecl m (HsPatBind loc pat rhs wbinds)
     = do (localdecls, renamings) <- delocalizeWhereBinds m wbinds
-         return $ localdecls ++ [alphaconvert m renamings (HsPatBind loc pat rhs (HsBDecls []))]
+         (rhs', morelocaldecls)  <- delocalizeRhs m (alphaconvert m renamings rhs)
+
+         return $ localdecls ++ morelocaldecls ++ [HsPatBind loc pat rhs' (HsBDecls [])]
 
 delocalizeDecl m (HsFunBind matchs)
     = do (matchs', localdecls) <- liftM (\(xs, ys) -> (xs, concat ys))
                                     $ mapAndUnzipM (delocalizeMatch m) matchs
          return (localdecls ++ [HsFunBind matchs'])
 
-delocalizeDecl m decl = return [decl]
+delocalizeDecl m decl = assert (check decl) $ return [decl]
+    where check decl = and [null (universeBi decl :: [HsBinds]),
+                            null (filter isHsLet (universeBi decl :: [HsExp]))]
+          isHsLet expr = case expr of HsLet _ _ -> True; _ -> False
 
 
 delocalizeMatch m (HsMatch loc name pats rhs wbinds)
     = do (localdecls, renamings) <- delocalizeWhereBinds m wbinds
-         return (alphaconvert m renamings (HsMatch loc name pats rhs (HsBDecls [])),
-                 localdecls)
+         (rhs', morelocaldecls)  <- delocalizeRhs m (alphaconvert m renamings rhs)
+         return (HsMatch loc name pats rhs' (HsBDecls []), 
+                localdecls ++ morelocaldecls)
 
 delocalizeWhereBinds m (HsBDecls localdecls)
     = do renamings <- renameToFreshIdentifiers (bindingsFromDecls m localdecls)
@@ -61,8 +69,22 @@ delocalizeWhereBinds m (HsBDecls localdecls)
          localdecls'' <- concatMapM (delocalizeDecl m) localdecls'
          return (localdecls'', renamings)
 
+delocalizeRhs m (HsUnGuardedRhs exp)
+    = do (exp', decls) <- delocalizeLets m exp; return (HsUnGuardedRhs exp', decls)
+
+delocalizeLets m (HsLet wbinds body)
+    = do (decls, renamings) <- delocalizeWhereBinds m wbinds
+         (body', moredecls) <- delocalizeLets m (alphaconvert m renamings body)
+         return (body', decls ++ moredecls)
+
+delocalizeLets m hsexp
+     = let (subexpressions, storer) = uniplate hsexp
+       in do (subexpressions', decls) <- liftM (\(xs, ys) -> (xs, concat ys))
+                                          $ mapAndUnzipM (delocalizeLets m) subexpressions
+             return (storer subexpressions', decls)
 
 renameToFreshIdentifiers :: [HsQName] -> State GensymCount [(HsQName, HsQName)]
 renameToFreshIdentifiers qnames
     = do freshs <- mapM genHsQName qnames
          return (zip qnames freshs)
+
