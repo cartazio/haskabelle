96e59530ddf36a6e43b8f87a29b1776935fc6d39
haftmann <devnull@localhost>
2007-12-12 06:42:08 +0000

    added examples

--- ex/src_hs/Classes.hs
+++ ex/src_hs/Classes.hs
@@ -1,1 +1,24 @@
 module Classes where
+
+class Monoid a where
+  nothing :: a
+  plus :: a -> a -> a
+
+instance Monoid Integer where
+  nothing = 0
+  plus = (+)
+
+summ :: (Monoid a) => [a] -> a
+summ [] = nothing
+summ (x:xs) = plus x (summ xs)
+
+class (Monoid a) => Group a where
+  inverse :: a -> a
+
+instance Group Integer where
+  inverse = negate
+
+pow :: (Group a) => Integer -> a -> a
+pow 0 _ = nothing
+pow k x = if k < 0 then pow (- k) (inverse x)
+  else plus x (pow (k - 1) x)
--- ex/src_hs/Integers.hs
+++ ex/src_hs/Integers.hs
@@ -1,1 +1,10 @@
 module Integers where
+
+sense :: Integer
+sense = 42
+
+sendling :: Integer
+sendling = 1705
+
+doodle :: Integer
+doodle = sense * sendling + sense
