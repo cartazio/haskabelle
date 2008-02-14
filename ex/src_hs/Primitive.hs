e412ba3620dcf47577abc04288f644f743a49302
rittweil <devnull@localhost>
2008-02-14 19:02:58 +0000

    * ex/src_hs/Primitive.hs:
        Added isEven and isOdd to test mutually recursive function
        definitions.

--- ex/src_hs/Primitive.hs
+++ ex/src_hs/Primitive.hs
@@ -13,3 +13,18 @@ incr n = Succ n
 
 decr :: Nat -> Nat
 decr (Succ n) = n
+
+
+-- The following is to test mutually-recursive function definitions.
+
+data Boolean = Verum | Falsum
+
+isEven :: Nat -> Boolean
+isEven Zero        = Verum
+isEven (Succ Zero) = Falsum
+isEven n           = isOdd (decr n)
+
+isOdd :: Nat -> Boolean
+isOdd Zero        = Falsum
+isOdd (Succ Zero) = Verum
+isOdd n           = isEven (decr n)
