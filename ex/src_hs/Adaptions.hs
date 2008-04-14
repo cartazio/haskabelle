3f768d071f016060fd470c3eae3d2ec04cf9b63d
haftmann <devnull@localhost>
2008-04-14 07:44:34 +0000

    -

---
+++ ex/src_hs/Adaptions.hs
@@ -0,0 +1,27 @@
+-- some rather fundamtenal adaptions
+
+module Adapations where
+
+implies :: Bool -> Bool -> Bool
+implies False _ = True
+implies True True = True
+implies True False = False
+
+nand :: Bool -> Bool -> Bool
+nand p q = not (p && q)
+
+nor :: Bool -> Bool -> Bool
+nor p q = not (p || q)
+
+append :: [a] -> [a] -> [a]
+append [] ys = ys
+append xs [] = xs
+append (x:xs) ys = x : append xs ys
+
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = append (rev xs) [x]
+
+who_am_i_smile :: (a -> b) -> Maybe a -> Maybe b
+who_am_i_smile f Nothing = Nothing
+who_am_i_smile f (Just x) = Just (f x)
--- importer/AdaptMapping.hs
+++ importer/AdaptMapping.hs
@@ -6,13 +6,16 @@ module Importer.AdaptMapping where
 import Importer.AdaptTable
 
 rawAdaptionTable = [(Haskell "Prelude.Bool" Type "Prelude.Bool", Isabelle "bool" Type "bool"),
+  (Haskell "Prelude.List" Type "Prelude.List", Isabelle "List.list" Type "List.list"),
+  (Haskell "Prelude.Maybe" Type "Prelude.Maybe", Isabelle "Datatype.option" Type "Datatype.option"),
   (Haskell "Prelude.True" Function "bool", Isabelle "True" Function "bool"),
   (Haskell "Prelude.False" Function "bool", Isabelle "False" Function "bool"),
-  (Haskell "Prelude.+" Function "'a => 'a => 'a", Isabelle "HOL.plus_class.plus" Function "'a => 'a => 'a"),
-  (Haskell "Prelude.-" Function "'a => 'a => 'a", Isabelle "HOL.minus_class.minus" Function "'a => 'a => 'a"),
-  (Haskell "Prelude.<=" Function "'a => 'a => bool", Isabelle "HOL.ord_class.less_eq" Function "'a => 'a => bool"),
-  (Haskell "Prelude.<" Function "'a => 'a => bool", Isabelle "HOL.ord_class.less" Function "'a => 'a => bool"),
+  (Haskell "Prelude.&&" Function "bool => bool => bool", Isabelle "op &" Function "bool => bool => bool"),
+  (Haskell "Prelude.||" Function "bool => bool => bool", Isabelle "op |" Function "bool => bool => bool"),
+  (Haskell "Prelude.[]" Function "'a list", Isabelle "List.list.Nil" Function "'a list"),
   (Haskell "Prelude.:" Function "'a => 'a list => 'a list", Isabelle "List.list.Cons" Function "'a => 'a list => 'a list"),
   (Haskell "Prelude.head" Function "'a list => 'a", Isabelle "List.hd" Function "'a list => 'a"),
   (Haskell "Prelude.tail" Function "'a list => 'a list", Isabelle "List.tl" Function "'a list => 'a list"),
-  (Haskell "Prelude.++" Function "'a list => 'a list => 'a list", Isabelle "List.append" Function "'a list => 'a list => 'a list")]
+  (Haskell "Prelude.++" Function "'a list => 'a list => 'a list", Isabelle "List.append" Function "'a list => 'a list => 'a list"),
+  (Haskell "Prelude.Nothing" Function "'a option", Isabelle "Datatype.option.None" Function "'a option"),
+  (Haskell "Prelude.Just" Function "'a ~=> 'a", Isabelle "Datatype.option.Some" Function "'a ~=> 'a")]
