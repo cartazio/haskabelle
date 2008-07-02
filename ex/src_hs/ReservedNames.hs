8120790c9e824c576c9a092c8fa5d3a8d0532bda
rittweil <devnull@localhost>
2008-07-02 20:44:26 +0000

    Update.

--- ex/src_hs/ReservedNames.hs
+++ ex/src_hs/ReservedNames.hs
@@ -6,4 +6,7 @@ foo nat set = nat : ([nat] ++ set)
 bar nat = let set = [nat] in set
 
 quux nat = frob nat []
-    where frob nat set = [nat] : set
+    where frob nat set = nat : set
+
+zurp x = knorks x []
+    where knorks x set = [x] ++ set
