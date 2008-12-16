0c7a8faf3cf3a79e51803ab963cb2eed6885042f
haftmann <devnull@localhost>
2008-12-16 16:05:38 +0100

    binary distribution

--- Main.hs
+++ Main.hs
@@ -11,11 +11,10 @@ import System.Exit (exitWith, ExitCode( ExitFailure))
 import Importer.FilePrinter (importProject, importFiles)
 import Importer.Configuration (readConfig)
 
-usage :: IO ()
-usage = do
-  executable <- getProgName
+usage :: String -> IO ()
+usage executableName = do
   putStrLn ""
-  putStrLn ("Usage: " ++ executable ++ " <SRC1> .. <SRCn> <DST> | --config <CONFIG>")
+  putStrLn ("Usage: " ++ executableName  ++ " <SRC1> .. <SRCn> <DST> | --config <CONFIG>")
   putStrLn ""
   putStrLn "  Import Haskell files <SRC1> .. <SRCn> into Isabelle theories in directory DST"
   putStrLn "    OR import Haskell files according to configuration file <CONFIG>"
@@ -23,13 +22,13 @@ usage = do
   exitWith (ExitFailure 1)
 
 mainInterface :: [String] -> IO ()
-mainInterface ["--config", configFile] = do
+mainInterface [executableName, "--config", configFile] = do
   config <- readConfig configFile
   importProject config
-mainInterface srcs_dst @ (_ : _ : _) =
+mainInterface (executableName : srcs_dst @ (_ : _ : _)) =
   importFiles (init srcs_dst) (last srcs_dst)
-mainInterface _ =
-  usage
+mainInterface [executableName] =
+  usage executableName 
 
 main :: IO ()
 main = getArgs >>= mainInterface
