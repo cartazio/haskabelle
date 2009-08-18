4a761455ab8122520c6051d6e857e1944a206224
haftmann <devnull@localhost>
2009-08-18 13:40:52 +0200

    dependency example

---
+++ ex/src_hs/Stmt_Dependencies.hs
@@ -0,0 +1,21 @@
+module Stmt_Dependencies where
+
+data Twin a b = Twin a b
+
+dest_Twin :: Twin a b -> (a, b)
+dest_Twin (Twin x y) = (x, y)
+
+f :: a -> (a, a)
+f x = dest_Twin (g x)
+
+g :: a -> Twin a a
+g x = Twin (h x) (h x)
+
+h :: a -> a
+h x = x
+
+{-double :: a -> Twin a a
+double x = Twin x x-}
+
+k :: (a, b) -> (a, b)
+k (x, y) = dest_Twin (Twin x y)
