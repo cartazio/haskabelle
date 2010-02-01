6437e57d874cf1b7e37249c92331a02cbc5f85e5
haftmann <devnull@localhost>
2010-02-01 09:24:16 +0100

    added record with type parameter

--- ex/src_hs/Records.hs
+++ ex/src_hs/Records.hs
@@ -1,17 +1,18 @@
 module Records where
 
-data MyRecord = A{aField1 :: String, common1 :: Bool, common2 :: Int}
-              | B{bField1 :: Bool, bField2 :: Int, common1 :: Bool, common2 :: Int}
+data Identity a = Id { this :: a }
+
+data MyRecord = A { aField1 :: String, common1 :: Bool, common2 :: Int }
+              | B { bField1 :: Bool, bField2 :: Int, common1 :: Bool, common2 :: Int }
               | C Bool Int String
 
 constr :: MyRecord
-constr = A{ aField1 = "foo", common1 = True}
-
+constr = A { aField1 = "foo", common1 = True }
 
 update :: MyRecord -> MyRecord
-update x = x{common2 = 1, common1 = False }
+update x = x { common2 = 1, common1 = False }
 
 pattern :: MyRecord -> Int
-pattern A{common2 = val} = val
-pattern B{bField2 = val} = val
+pattern A { common2 = val } = val
+pattern B { bField2 = val } = val
 pattern (C _ val _) = val
