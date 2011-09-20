aa1be1d7c8e71fbc597205fb0b074e76569c8d4e
noschinl <devnull@localhost>
2011-09-20 12:56:42 +0200

    turn Haskabelle into an Isabelle component

--- Main.hs
+++ Main.hs
@@ -15,29 +15,32 @@ import Importer.Adapt (readAdapt, Adaption (..))
 import Importer.Configuration (readConfig)
 import Importer.Version (version)
 
-usage :: String -> IO ()
-usage executableName = do
-  putStrLn ("This is " ++ version ++ ".")
-  putStrLn ""
-  putStrLn ("Usage: " ++ executableName  ++ " [--adapt <ADAPT>] <SRC1> .. <SRCn> <DST> | --config <CONFIG>")
-  putStrLn ""
-  putStrLn "  Import Haskell files <SRC1> .. <SRCn> into Isabelle theories in directory DST,"
-  putStrLn "    optionally using customary adaption in directory <ADAPT>"
-  putStrLn "    OR import Haskell files according to configuration file <CONFIG>"
-  putStrLn ""
-  exitWith (ExitFailure 1)
+{-
+  Usage of the haskabelle binary:
+
+  haskabelle_bin [--adapt <ADAPT>] <SRC1> .. <SRCn> <DST> | --config <CONFIG>
+
+  Import Haskell files <SRC1> .. <SRCn> into Isabelle theories in directory
+  <DST>, optionally using customary adaption in directory <ADAPT> OR import
+  Haskell files according to configuration file <CONFIG>.
+-}
 
 mainInterface :: [String] -> IO ()
-mainInterface [executableName, defaultAdaptDir, "--config", configFile] = do
+mainInterface ["--internal", defaultAdaptDir, "--config", configFile] = do
   config <- readConfig configFile
   importProject config defaultAdaptDir
-mainInterface (executableName : _ : "--adapt" : adaptDir : srcs_dst @ (_ : _ : _)) =
+mainInterface ("--internal" : _ : "--adapt" : adaptDir : srcs_dst @ (_ : _ : _)) =
   importFiles adaptDir (init srcs_dst) (last srcs_dst)
-mainInterface (executableName : defaultAdaptDir : srcs_dst @ (_ : _ : _)) =
+mainInterface ("--internal" : defaultAdaptDir : srcs_dst @ (_ : _ : _)) =
   importFiles defaultAdaptDir (init srcs_dst) (last srcs_dst)
-mainInterface (executableName : _) =
-  usage executableName 
-mainInterface [] = do
+mainInterface ("--internal" : "--version" : _) = do
+  putStrLn (version ++ ".")
+
+mainInterface ("--internal" : args) = do
+  putStrLn "Error calling internal haskabelle binary. Wrong parameters:"
+  putStrLn ("  " ++ show args)
+
+mainInterface _ = do
   putStrLn "Do not invoke linked Haskabelle binary directly"
   putStrLn "  -- invoke it as described in the Haskabelle manual."
   putStrLn ""
